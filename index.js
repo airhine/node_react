8420a3c074dcea0b2911cb9ffbf2d4ef3f59dec8
airhine <taeho7702@gmail.com>
2025-09-29 21:19:37 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -2,9 +2,25 @@ const express = require('express');
 const app = express()
 const port = 5000
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://airhine:1q2w3e4r@nodereact.l5fw2ub.mongodb.net/?retryWrites=true&w=majority&appName=nodereact', {  
-}).then(() => console.log('connected'))
+const bodyParser = require('body-parser');
+const config = require('./config/key');
+const {User} = require('./models/User');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
+mongoose.connect(config.mongoURI, {  
+}).then(() => console.log('MongoDB connected'))
 .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World! test'))
+
+app.post('/register', (req, res) =>{
+    const user = new User(req.body)
+     user.save()
+        .then((userInfo) => res.status(200).json({ success: true }))
+        .catch((err) => res.json({ success: false, err }));
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
